00ab348cd7b9f7476c1539a96c3d25983006e49d
Your Name <vahiba1345@gmail.com>
2025-06-03 20:41:17 +0330

    fix: remove system dependencies from build script

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y gdal-bin libgdal-dev python3-gdal binutils libproj-dev
-
 # Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
